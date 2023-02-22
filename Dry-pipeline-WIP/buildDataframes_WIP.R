26508ae3dea4d7a7b2af065f1c367919c63f47fb
Sophia Horigan <sophia.horigan@gmail.com>
2023-02-22 17:10:30 -0600

    First workthrough of clock building pipeline
    Successful! Needs cleaning/trimming, and there are many decisions to be made regarding dataset filtering. But the structural pieces are in place.

--- Dry-pipeline-WIP/buildDataframes_WIP.R
+++ Dry-pipeline-WIP/buildDataframes_WIP.R
@@ -28,17 +28,24 @@
 rm(list=ls())
 
 #Set working directory - add yours here
-homewd= "/Users/theresalaverty/Documents/R/R_repositories/Epigenetic_Clock"
+#homewd= "/Users/theresalaverty/Documents/R/R_repositories/Epigenetic_Clock"
+homewd= "/Users/shorigan/Documents/GitHub/Epigenetic_Clock"
 #Folder where the bismark.cov files of interest are stored
 setwd(paste0(homewd,"/methylation-aging/output/bismark_bedgraph/"))
 
 #Load packages
 #install.packages("data.table")
 #install.packages("dplyr")
+#install.packages("janitor")
 library(data.table)
 library(dplyr)
-####################################################################################
+library(tidyr)
+library(tidyverse)
+library(janitor)
+library(cowplot)
 
+####################################################################################
+#PART 1: TRANSFORM METHYLATION DATA
 ### 1. CpG sites ###
 #find all of the *-sub_CpG.gz.bismark.cov files in the working directory
 (bismark_files_cpg <- list.files(pattern="*-sub_CpG.gz.bismark.cov", recursive=TRUE))
@@ -121,7 +128,7 @@ write.csv(out_chg, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/sub_chg_mer
 
 
 ####################################################################################
-
+# PART 2: GENERATE BREADTH COVERAGE STATISTICS
 # Suggested next steps from Manny; "Unlike in Wilkinson, we have nuance that makes 
 # things trickier. My first stab would be to do a correlation graph between the three 
 # methylation sites per probe to see how or if they differ. Then get the weighed 
@@ -133,30 +140,150 @@ write.csv(out_chg, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/sub_chg_mer
 #CALCULATE:
 #1. CpG sites
 #Proportion of samples with methylation data (i.e., NOT NA) per probe site
-prop_cpg <- colSums(!is.na(out_cpg[,-1]))/nrow(out_cpg)
+out_cpg_cov <- out_cpg[-(1:8),]
+#Proportion of samples with methylation data (i.e., NOT NA) per probe site
+prop_cpg <- as.numeric(colSums(!is.na(out_cpg[,-1]))/nrow(out_cpg))
 #Average percent methylation per probe site (removes NAs)
 mean_cpg <- colMeans(out_cpg[,-1], na.rm=T)
 #Append data to the dataframe
-out_cpg[nrow(out_cpg) + 1,] = c("proportion_coverage",prop_cpg)
-out_cpg[nrow(out_cpg) + 1,] = c("mean_percent_methylation",mean_cpg)
-View(out_cpg)
+out_cpg_cov[nrow(out_cpg_cov)+1, ] <- c("proportion_coverage", prop_cpg)
+out_cpg_cov[nrow(out_cpg_cov)+1, ] <- c("mean_percent_methylation", mean_cpg)
+View(out_cpg_cov)
+
+write.csv(out_cpg_cov, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/sub_cpg_merged_cov.csv"), row.names = F)
 
 #2. CHH sites
 #Proportion of samples with methylation data (i.e., NOT NA) per probe site
-prop_chh <- colSums(!is.na(out_chh[,-1]))/nrow(out_chh)
+out_chh_cov <- out_chh[-(1:8),]
+#Proportion of samples with methylation data (i.e., NOT NA) per probe site
+prop_chh <- as.numeric(colSums(!is.na(out_chh[,-1]))/nrow(out_chh))
 #Average percent methylation per probe site (removes NAs)
 mean_chh <- colMeans(out_chh[,-1], na.rm=T)
 #Append data to the dataframe
-out_chh[nrow(out_chh) + 1,] = c("proportion_coverage",prop_chh)
-out_chh[nrow(out_chh) + 1,] = c("mean_percent_methylation",mean_chh)
-View(out_chh)
+out_chh_cov[nrow(out_chh_cov)+1, ] <- c("proportion_coverage", prop_chh)
+out_chh_cov[nrow(out_chh_cov)+1, ] <- c("mean_percent_methylation", mean_chh)
+View(out_chh_cov)
+
+write.csv(out_chh_cov, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/sub_chh_merged_cov.csv"), row.names = F)
+
 
 #3. CHG sites
+out_chg_cov <- out_chg[-(1:8),]
 #Proportion of samples with methylation data (i.e., NOT NA) per probe site
-prop_chg <- colSums(!is.na(out_chg[,-1]))/nrow(out_chg)
+prop_chg <- as.numeric(colSums(!is.na(out_chg[,-1]))/nrow(out_chg))
 #Average percent methylation per probe site (removes NAs)
 mean_chg <- colMeans(out_chg[,-1], na.rm=T)
 #Append data to the dataframe
-out_chg[nrow(out_chg) + 1,] = c("proportion_coverage",prop_chg)
-out_chg[nrow(out_chg) + 1,] = c("mean_percent_methylation",mean_chg)
-View(out_chg)
+out_chg_cov[nrow(out_chg_cov)+1, ] <- c("proportion_coverage", prop_chg)
+out_chg_cov[nrow(out_chg_cov)+1, ] <- c("mean_percent_methylation", mean_chg)
+View(out_chg_cov)
+
+write.csv(out_chg_cov, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/sub_chg_merged_cov.csv"), row.names = F)
+
+
+# PLOTS OF BREADTH COVERAGE STATISTICS
+# CPG
+out_cpg_cov_plot <- as.data.frame(t(out_cpg_cov)) %>%
+  row_to_names(row_number=1) %>%
+  mutate(across(everything(), as.numeric))
+
+p1 <- ggplot(out_cpg_cov_plot, aes(proportion_coverage)) + geom_histogram(binwidth = 0.125) + 
+  labs(title ="CPG coverage - 8 samples") + scale_x_continuous(breaks = c(0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1)) #number of bats
+p1
+p2 <- ggplot(out_cpg_cov_plot, aes(mean_percent_methylation)) + geom_histogram() + 
+  labs(title ="CPG percent methylation - 8 samples") + scale_x_continuous(breaks = c(0,10,20,30,40,50,60,70,80,90,100))
+p2
+
+# CHG
+out_chg_cov_plot <- as.data.frame(t(out_chg_cov)) %>%
+  row_to_names(row_number=1) %>%
+  mutate(across(everything(), as.numeric))
+
+p3 <- ggplot(out_chg_cov_plot, aes(proportion_coverage)) + geom_histogram(binwidth = 0.125) + 
+  labs(title ="CHG coverage - 8 samples") + scale_x_continuous(breaks = c(0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1)) #number of bats
+p3
+p4 <- ggplot(out_chg_cov_plot, aes(mean_percent_methylation)) + geom_histogram() + 
+  labs(title ="CHG percent methylation - 8 samples") + scale_x_continuous(breaks = c(0,10,20,30,40,50,60,70,80,90,100))
+p4
+
+# CHH
+out_chh_cov_plot <- as.data.frame(t(out_chh_cov)) %>%
+  row_to_names(row_number=1) %>%
+  mutate(across(everything(), as.numeric))
+
+p5 <- ggplot(out_chh_cov_plot, aes(proportion_coverage)) + geom_histogram(binwidth = 0.125) + 
+  labs(title ="CHH coverage - 8 samples") + scale_x_continuous(breaks = c(0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1)) #number of bats
+p5
+p6 <- ggplot(out_chh_cov_plot, aes(mean_percent_methylation)) + geom_histogram() + 
+  labs(title ="CHH percent methylation - 8 samples") + scale_x_continuous(breaks = c(0,10,20,30,40,50,60,70,80,90,100))
+p6
+
+plot_grid(p1, p2, p3, p4, p5, p6, ncol = 2)
+
+####################################################################################
+# PART 3: CALCULATE DEPTH COVERAGE STATISTICS
+
+# Goal: use fastq files to figure out the coverage depth
+# for all sites, to be used for filtering
+
+
+
+
+
+####################################################################################
+# PART 4: FILTERING BASED ON BREADTH AND DEPTH COVERAGE STATISTICS
+
+# Goal: only keep sites that map to at least 1 genome (this criteria may already be in place in bismark) 
+# this might need to be 2? based on how training works # look into wilkinson training
+# and that are above a read depth threshhold
+# 
+
+# Goal: after determining coverage filtering based on 
+# statistics generated above, generate a list of 
+# sites to keep. Then use this list as a reference
+# to trim the original dataframes to only contain
+# those sites and their methylation data.
+# This will create the final list of sites to be used
+# for clock training.
+
+
+
+
+##################################################################################
+# PART 5: Add in metadata
+
+# Goal: to make this dataframe ready for clock building, we need to
+# add back in associated metadata for each sample.
+# This includes the age, method of aging, and tissue type
+
+# Because we are troubleshooting this on the myotis samples which
+# do not have known ages, I am just going to make up ages
+# in order to practice making the clocks
+
+cpg_clock <- out_cpg
+chg_clock <- out_chg
+chh_clock <- out_chh
+
+cpg_clock$known_age <- c(1,3,4,5,5,2,1,6)
+cpg_clock$id <- c("M_californicus", "M_evotis", "M_evotis", "M_lucifugus", "M_lucifugus", "M_lucifugus", "M_thysanodes", "M_volans") #How do we want these to be written? just spp name? (to be able to build spp clocks, might need to add 'indl' column)
+cpg_clock$tissue <- c("wingpunch", "wingpunch", "wingpunch", "cellline", "cellline", "cellline", "cellline", "cellline") #these we do know
+cpg_clock$age_method <- c("DNAm", "DNAm", "DMAm", "markrecap", "dentition", "dentition", "dentition", "dentition") #these are all DNAm technically, but adding in some dentition and mark recap to test
+write.csv(cpg_clock, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/cpg_clock.csv"), row.names = F)
+
+chg_clock$known_age <- c(3,5,3,1,1,2,2,3)
+chg_clock$id <- c("M_californicus", "M_evotis", "M_evotis", "M_lucifugus", "M_lucifugus", "M_lucifugus", "M_thysanodes", "M_volans") #How do we want these to be written? just spp name? (to be able to build spp clocks, might need to add 'indl' column)
+chg_clock$tissue <- c("wingpunch", "wingpunch", "wingpunch", "cellline", "cellline", "cellline", "cellline", "cellline") #these we do know
+chg_clock$age_method <- c("DNAm", "DNAm", "DMAm", "markrecap", "dentition", "dentition", "dentition", "dentition") #these are all DNAm technically, but adding in some dentition and mark recap to test
+write.csv(chg_clock, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/chg_clock.csv"), row.names = F)
+
+chh_clock$known_age <- c(5,6,7,8,7,6,7,8)
+chg_clock$id <- c("M_californicus", "M_evotis", "M_evotis", "M_lucifugus", "M_lucifugus", "M_lucifugus", "M_thysanodes", "M_volans") #How do we want these to be written? just spp name? (to be able to build spp clocks, might need to add 'indl' column)
+chh_clock$tissue <- c("wingpunch", "wingpunch", "wingpunch", "cellline", "cellline", "cellline", "cellline", "cellline") #these we do know
+chh_clock$age_method <- c("markrecap", "DNAm", "DMAm", "markrecap", "markrecap", "dentition", "dentition", "dentition") #these are all DNAm technically, but adding in some dentition and mark recap to test
+write.csv(chh_clock, file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/chh_clock.csv"), row.names = F)
+
+# need to figure out how to bind together into a giant dataframe
+
+############
+# BY NOW, WE SHOULD HAVE DATAFRAMES THAT ARE READY TO BE USED IN CLOCK TRAINING
+
--- Dry-pipeline-WIP/trainTest_Clocks_WIP.R
+++ Dry-pipeline-WIP/trainTest_Clocks_WIP.R
@@ -1,4 +1,11 @@
+##################################################################################
+# TRAINING CLOCK
+# This is code to train an epigenetic clock
+# it is currently built to use Myotis samples (see buildDataframes_WIP)
+# email shorigan@uchicago.edu, tlaverty@nmsu.edu, or cbrook@uchicago.edu with questions
 
+
+# Note from Griffin et al
 # The dataframe (df) should be organized with rows = samples, columns = CpGs,... 
 # ...and a few metadata columns that need to be removed before clock training.
 
@@ -9,76 +16,114 @@
 # (4) Predict ages with the model in the testing set
 # (5) Gather stats, write clock coefficeints, plot results.
 
-trainTest_clock <- function(df,seed,alpha,tissues) {
+##################################################################################
+# PART 1: SET UP ENVIRONMENT
+# Clear work environment
+rm(list=ls())
+
+# Set working directory - add yours here
+#homewd= "/Users/theresalaverty/Documents/R/R_repositories/Epigenetic_Clock"
+homewd= "/Users/shorigan/Documents/GitHub/Epigenetic_Clock"
+# Folder where the clock site csvs are stored
+setwd(paste0(homewd,"/Dry-pipeline-WIP/Dataframes/"))
+
+# load packages
+#install.packages("glmnet")
+#install.packages("ggpubr")
+library(glmnet)
+library(readr)
+library(ggpubr)
+library(dplyr)
+
+# read in files - one for each type of site (for now)
+# CPG clock sites
+cpg_clock_sites <- read.csv(file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/cpg_clock.csv"), header = T)
+# CHG clock sites
+chg_clock_sites <- read.csv(file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/chg_clock.csv"), header = T)
+# CHH clock sites
+chh_clock_sites <- read.csv(file= paste0(homewd,"/Dry-pipeline-WIP/Dataframes/chh_clock.csv"), header = T)
+
+# set clock parameters
+# random seed
+seed = 777 # does this need to be generated new each time, probably yes
+# alpha for elastic net regression (0-1) # Wilkinson used 0.5, Griffin used 0.05 or 0.1 # can also try to fit this value
+a = 0.5
+
+df <- cpg_clock_sites
+# NEED TO CHANGE
+#just for now to get the clock working-- removing columns (sites) with 7 or more NAs
+df <- df[,colSums(is.na(df)) < nrow(df)-2]
+
+#trainTest_clock <- function(df,seed,a) {
   
-  print(seed) #random number 
+  print(seed)
   set.seed(seed)
-  print(alpha) #elastic net regression parameter (between lasso and ridge) #Wilkinson used 0.5, Griffin used 0.05 or 0.1
-  #you can refine and train alpha -- see notes in paper
-  
-  tissue_names <- paste(c(tissues), collapse='_' ) #we don't have tissues so this is irrelevant?
-  #they built clocks based on different tissues, so our 'tissue' of interest is just dentition
-  
+  print(a)
+
+  # create subsets of the data to be used for differential clock building # actually, I think it's just to ensure random sampling for clock building
+  # optional! # make one for CPG, CHH, CGH in future
+  tissue_names <- paste(c(df$tissue)) #we don't have tissues so this is irrelevant?
+  age_methods <- paste(c(df$age_method))
+
   ##DISCRETIZE AGE GROUPS
-  #filtering for the tissues of interest and assigning discrete age-groups.
-  #we need to just assign discrete age-groups
-  x <- df %>%
-    filter(Tissue %in% tissues) %>% #shouldn't need to do this either, just do it by age year
-    mutate(age_group = case_when(Age_months<2.5 ~ "3", #we do have indls less than one year
-                                 Age_months>=3 & Age_months<6 ~"3-6",
-                                 Age_months>=6 & Age_months<10 ~"6-10",
-                                 Age_months>=10 & Age_months<14 ~"10-14",
-                                 Age_months>=14 & Age_months<18 ~"14-18",
-                                 Age_months>=18 & Age_months<24 ~"18-24",
-                                 Age_months>=24 & Age_months<28 ~"24-28",
-                                 Age_months>=28 & Age_months<32 ~"28-32",
-                                 Age_months>=32 & Age_months<34 ~"32-34",
-                                 Age_months>=34~">34")) 
+  # see code below, not using for now
   
   tr_fr <- 0.8 # the training samples randomly selected from each age_group
   ts_fr <- (1-tr_fr) #testing fraction
   
-  training_samples <- x %>% filter(sample=="none") #keeps rows with sample == none #bc those are un-dated? #or already subset?
+  training_samples <- df %>% filter(id=="none") # creates empty dataframe of correct size
   
   ##SUBSET TRAINING SET
-  for (j in  unique(x$Tissue)) { #don't need this loop
+  for (j in  unique(df$tissue)) { #don't need this loop
     #selecting samples from initial age group to start building training matrix
-    filtered <- x %>%  # filtered is age group
-      filter(age_group==unique(x$age_group)[1] & Tissue==j) #filter from age group 1, tissue type 1
+    filtered <- df %>%  # filtered is age group
+      filter(known_age==unique(df$known_age)[1] & tissue==j) #filter from age group 1, tissue type 1
     tr_samps <- filtered %>% #tr_samps is training 80% of age group
-      sample_n(as.integer((tr_fr*length(age_group)))) #sample training proportion from group (based on tr_fr)
+      sample_n(as.integer((tr_fr*length(known_age)))) #sample training proportion from group (based on tr_fr)
     
     #now selecting samples from each age group
-    for (i in unique(x$age_group)[2:length(unique(x$age_group))]){ # now subset the rest of the age groups one by one
-      f <- x %>%  # f is age group
-        filter(age_group==i & Tissue==j)
+    for (i in unique(df$known_age)[2:length(unique(df$known_age))]){ # now subset the rest of the age groups one by one
+      f <- df %>%  # f is age group
+        filter(known_age==i & tissue==j)
       s <- f %>% # s is 80% of f
-        sample_n(as.integer((tr_fr*length(age_group))+0.5)) #and randomly sample the rest of the training data
+        sample_n(as.integer((tr_fr*length(known_age))+0.5)) #and randomly sample the rest of the training data
       tr_samps <- bind_rows(tr_samps,s) #bind s (training for looped age group) to tr_samps (training for age group 1)
     }
     training_samples <- bind_rows(training_samples,tr_samps) # bind everything to make final df of training samples
   }
   
   #FORMAT DATA SETS FOR CLOCK ANALYSIS
+  ### ISSUE WHEN SPLITTING TRAIN AND TEST-- the split leaves train data with some cpgs that have all NA's
+  # need to either filter secondarily (then go back and re-sample test.. seems like a hassle)
+  # or remove all sites where only one sample hit the probe and hope the split/test works
+  # this will be as an issue with larger sample sizes as well. Creating a more stringent cutoff is necessary I think.?
+  
   #training data
-  #y is predictor, which age Age in months
-  y.train <- training_samples %>% select(Age_months) #takes only the age 
-  y.train <- as.matrix(y.train) 
-  #x is independent, which are methylation levels at CpG sites
-  x.train <- training_samples %>% select(-sample,-Age_months,-age_group,-Tissue) #drop sample, Age, age group, Tissue #which leaves CPGS?
-  x.train <- model.matrix(  ~ ., x.train) #makes a 'model matrix', which is a matrix of values of explanatory variables #basically all the explanatory variables and observations
+
+  y.train <- training_samples %>% select(known_age)
+  y.train <- as.matrix(y.train)
+  
+  x.train <- training_samples %>% select(-id,-known_age,-tissue,-age_method) #drop sample, Age, age group, Tissue #which leaves CPGS?
+  x.train <- as.data.frame(x.train)
+  x.train <- makeX(x.train, na.impute=TRUE) # this generates an average for the NA sites. maybe better to do elsewhere
   
   #testing data
-  x.ts <- x %>% filter(sample %in% training_samples$sample == F) #keep the data that wasn't used in the training set #still don't understand if he put 'none' in there or what
-  y.test <- x.ts %>% select(Age_months)
+  df.ts <- df %>% filter(id %in% training_samples$id == F) # need to make sure samples are individually id'd
+  y.test <- df.ts %>% select(known_age)
   y.test <- as.matrix(y.test) 
-  x.test <- x.ts %>% select(-sample,-Age_months,-age_group,-Tissue) 
-  x.test <- model.matrix( ~ ., x.test)
+  
+  x.test <- df.ts %>% select(-id,-known_age,-tissue,-age_method)
+  x.test <- as.data.frame(x.test)
+  #x.test <- model.matrix( ~ ., x.test)
+  x.test <- makeX(x.test, na.impute = TRUE)  # not sure exactly what sparse does
+  
+
   
   ## MODELING
-  a <- alpha # alpha parameter for elastic net regression
+  a <- 0.5 # alpha parameter for elastic net regression
+  set.seed(42) # do we need this part twice?
   
-  set.seed(42)
+  
   #fit a generalized linear model via penalized maximum likelihood
   #using cross validation (cv)
   cv.glmnet.fit <- cv.glmnet(x.train,y.train, #observations, response
@@ -90,6 +135,7 @@ trainTest_clock <- function(df,seed,alpha,tissues) {
   cv.glmnet.predicted <- predict(cv.glmnet.fit,
                                  s = cv.glmnet.fit$lambda.1se, #values of penalty parameter lambda (default is use entire sequence used to create the model, what he's doing here)
                                  newx = x.test ) #matrix of new values for x at which predictions are to be made
+  
   ##IS THIS JUST HOW I USE AN ALREADY FIT CLOCK???
   ##just needs the output of the fit (coefficients?) and lambda-- is that somewhere?
   
@@ -106,23 +152,23 @@ trainTest_clock <- function(df,seed,alpha,tissues) {
   
   # compile: model prediction of age, actual age, metadata (sample, tissue)
   train <- tibble(prediction = predicted.train,
-                  AGE=training_samples$Age_months, #what were the actual ages
-                  sample=training_samples$sample, #ID? or CpG site?
+                  AGE=training_samples$known_age, 
+                  sample=training_samples$id, 
                   set="train",
-                  Tissue=training_samples$Tissue) #don't need
+                  Tissue=training_samples$tissue) 
   test <- tibble(prediction = predicted.test, #predicted ages
-                 AGE= y.test, #actual ages
-                 sample=x.ts$sample,
+                 AGE= y.test, 
+                 sample=df.ts$id,
                  set="test",
-                 Tissue=x.ts$Tissue)
+                 Tissue=df.ts$tissue)
   # combine trained and tested predictions
-  all_prds <- bind_rows(train,test) %>% mutate(set=factor(set,levels = c("train","test")))
+  all_prds <- as.data.frame(bind_rows(train,test) %>% mutate(set=factor(set,levels = c("train","test"))))
   #export data
-  write_tsv(all_prds, path=paste(wd,'clock_optimization/predictions_',
-                                 tissue_names,
-                                 '_seed=',seed,'_alpha=',a,
-                                 '_train=',100*tr_fr,"_test=",
-                                 100*ts_fr,".tsv",sep=""))
+  write.csv(all_prds, file= paste0(homewd,'/Dry-Pipeline-WIP/clock_optimization/predictions_',
+                                   tissue_names,
+                                   '_seed=',seed,'_alpha=',a,
+                                   '_train=',100*tr_fr,"_test=",
+                                   100*ts_fr,".tsv",sep=""))
   
   #calculating stats from prediction
   p_mse <- mean((y.test - cv.glmnet.predicted)^2)
@@ -136,13 +182,14 @@ trainTest_clock <- function(df,seed,alpha,tissues) {
   p <- tibble(seed=seed, alpha=a, mse = p_mse, mae = p_mae, cor=c, rho=rh, mdae=p_med)
   predictions <- bind_rows(predictions,p) %>% filter(is.na(alpha) == F) 
   tmp_coeffs <- coef(cv.glmnet.fit, s="lambda.1se")
-  clock_1 <- data.frame(name=tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1],
+  clock1 <- data.frame(name=tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1],
                         coefficient = tmp_coeffs@x)
   
-  write_tsv(clock_1, path=paste(wd,'clock_optimization/',tissue_names,
-                                '_seed=',seed,'_alpha=',a,
-                                '_train=',100*tr_fr,"_test=",
-                                100*ts_fr,".tsv",sep=""))
+  write.csv(clock1, file= paste0(homewd,'/Dry-Pipeline-WIP/clock_optimization/',tissue_names,
+                                 '_seed=',seed,'_alpha=',a,
+                                 '_train=',100*tr_fr,"_test=",
+                                 100*ts_fr,".tsv",sep=""))
+  
   
   g <- all_prds %>%
     ggplot() +
@@ -153,9 +200,9 @@ trainTest_clock <- function(df,seed,alpha,tissues) {
     geom_abline(slope=1,linetype="dashed") +
     stat_cor(aes(AGE,prediction),vjust=0.1) +
     stat_cor(aes(AGE,prediction,color=Tissue)) +
-    ggtitle(paste("MdAE=", p_med, " N_CpGs=",length(clock_1$coefficient))) +
-    xlim(-10,40) +
-    ylim(-10,40)
+    ggtitle(paste("MdAE=", p_med, " N_CpGs=",length(clock1$coefficient))) +
+    xlim(0,8) +
+    ylim(0,8)
   print(g)
   ggsave(plot=g,
          height=5,
@@ -166,4 +213,22 @@ trainTest_clock <- function(df,seed,alpha,tissues) {
   
   return(predictions)
   print(predictions)
-}
+#}
+  
+  
+  
+  ## DISCRETIZE AGE GROUPS
+  #filtering for the tissues of interest and assigning discrete age-groups.
+  #we need to just assign discrete age-groups
+  x <- df %>%
+    filter(Tissue %in% tissues) %>% #shouldn't need to do this either, just do it by age year
+    mutate(age_group = case_when(Age_months<2.5 ~ "3", #we do have indls less than one year
+                                 Age_months>=3 & Age_months<6 ~"3-6",
+                                 Age_months>=6 & Age_months<10 ~"6-10",
+                                 Age_months>=10 & Age_months<14 ~"10-14",
+                                 Age_months>=14 & Age_months<18 ~"14-18",
+                                 Age_months>=18 & Age_months<24 ~"18-24",
+                                 Age_months>=24 & Age_months<28 ~"24-28",
+                                 Age_months>=28 & Age_months<32 ~"28-32",
+                                 Age_months>=32 & Age_months<34 ~"32-34",
+                                 Age_months>=34~">34")) 
